df32a3d4c0df864c3866dd7de5a9c624603c60c0
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-16 17:59:38 +0100

    fix pystyle

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,4 +10,4 @@ if n > 5:
 elif n == 0:
     print("Last digit of", number, "is", n, "and is 0")
 else:
-    print("Last digit of", number, "is", n, "and is less than 6 and not 0")  
+    print("Last digit of", number, "is", n, "and is less than 6 and not 0")
