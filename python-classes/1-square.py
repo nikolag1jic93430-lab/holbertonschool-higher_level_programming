8d42066f45c824dd6e90e68b2584253d7f335a60
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-28 15:27:57 +0100

    task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" class Square that defines a square """
 
 
 class Square:
-    """ square class """
     def __init__(self, size):
-        """ initialise size of square """
         self.__size = size
