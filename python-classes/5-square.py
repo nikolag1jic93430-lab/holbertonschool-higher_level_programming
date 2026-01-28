94e9e07c30f75a04635a9cf4c82905614a523d2a
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-28 15:28:52 +0100

    task5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -3,20 +3,16 @@
 
 
 class Square:
-    """ square class """
 
     def __init__(self, size=0):
-        """ initialise size of square """
         self.size = size
 
     @property
     def size(self):  # getter permet de lire une valeur privée
-        """ get current size """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Set the size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if isinstance(value, int) and value < 0:
@@ -24,11 +20,9 @@ class Square:
         self.__size = value
 
     def area(self):
-        """ Return current square area """
         return (self.__size * self.__size)
 
     def my_print(self):
-        """ print square """
         if self.__size == 0:
             print()
         else:
