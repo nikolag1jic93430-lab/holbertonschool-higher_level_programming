0902867abde1bd02a0284c1b4ce00f0c0375e5bb
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-28 15:29:06 +0100

    task6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-""" class Square that defines a square """
 
 
 class Square:
-    """ square class """
 
     def __init__(self, size=0, position=(0, 0)):
-        """ initialise size of square """
         self.size = size
         self.position = position
 
     @property
     def size(self):  # getter permet de lire une valeur privée
-        """ get current size """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Set the size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if isinstance(value, int) and value < 0:
@@ -26,7 +21,6 @@ class Square:
 
     @property
     def position(self):
-        """ get current position """
         return self.__position
 
     @position.setter
@@ -40,11 +34,9 @@ class Square:
         self.__position = value
 
     def area(self):
-        """ Return current square area """
         return (self.__size * self.__size)
 
     def my_print(self):
-        """ print square """
         if self.__size == 0:
             print()
             return
