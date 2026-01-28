eb0e94c625bc226481a4bae903730fedf951f41f
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-28 15:28:21 +0100

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-""" class Square that defines a square """
-
 
 class Square:
-    """ square class """
 
     def __init__(self, size=0):
-        """ initialise size of square """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if isinstance(size, int) and size < 0:
