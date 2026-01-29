88ea9deaf300798fdba74276700e00901a8b49f0
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-29 15:50:09 +0100

    task1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -30,4 +30,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__height = value
