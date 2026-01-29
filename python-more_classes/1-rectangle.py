d32a78833930b8271e3084318103913dbcc5a52c
Nikola Gajic <vediumtv93@gmail.Com>
2026-01-29 15:53:02 +0100

    correction remplace width par height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    """ Class """
+    """ Class rectangle """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -29,5 +29,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
