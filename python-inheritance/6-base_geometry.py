370768cf013e6df80206d9e34e9b84de81a23d8b
Nikola Gajic <vediumtv93@gmail.Com>
2026-02-05 11:44:17 +0100

    task 5 6 7

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+
+    def area(self):
+        """ area method """
+        raise Exception("area() is not implemented")
