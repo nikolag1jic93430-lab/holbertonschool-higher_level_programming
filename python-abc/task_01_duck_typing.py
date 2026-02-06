1d12a1ecbc13e29a00cd325ffdd5db55321fb8d5
Nikola Gajic <vediumtv93@gmail.Com>
2026-02-06 16:07:10 +0100

    task1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,7 +19,8 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * abs(self.radius) ** 2
+
 
     def perimeter(self):
         return 2 * math.pi * self.radius
