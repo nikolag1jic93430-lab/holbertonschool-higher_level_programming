15f9994c86274e8de74eade8462017213b58b066
Nikola Gajic <vediumtv93@gmail.Com>
2026-02-12 22:37:58 +0100

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a Student class """
+
+
+class Student:
+    """Represents a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student.
+
+        If attrs is a list of strings,
+        """
+        if isinstance(attrs, list) and all(
+                isinstance(attr, str) for attr in attrs):
+            return {
+                key: getattr(self, key)
+                for key in attrs
+                if hasattr(self, key)
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student
+        instance with values from json."""
+        for key, value in json.items():
+            setattr(self, key, value)
