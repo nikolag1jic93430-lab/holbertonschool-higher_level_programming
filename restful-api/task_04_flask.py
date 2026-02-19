ca22542c26f885f5d8d8953e9228fbaf2761b4d2
Nikola Gajic <vediumtv93@gmail.Com>
2026-02-19 16:51:39 +0100

    task 04

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+users = {}
+
+@app.route('/')
+def home():
+    """Return welcome message"""
+    return "Welcome to the Flask API!"
+
+@app.route('/status')
+def status():
+    """Return API status"""
+    return "OK"
+
+@app.route('/data')
+def get_usernames():
+    """Return list of all usernames"""
+    return jsonify(list(users.keys()))
+
+@app.route('/users/<username>')
+def get_user(username):
+    """Return full user info or error"""
+    if username in users:
+        return jsonify(users[username])
+    return jsonify({"error": "User not found"}), 404
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """Add a new user from JSON data"""
+    data = request.get_json()
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city")
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
+
+if __name__ == '__main__':
+    app.run()
