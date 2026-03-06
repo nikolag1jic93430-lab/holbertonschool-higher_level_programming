2b4cda2b5b8c1c37654eb9a461455e2481fe5350
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2026-03-06 11:46:36 +0100

    sql p2 T4 T5

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Create a table where id cannot be null
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- create a teble that has a unique id
+CREATE TABLE IF NOT EXISTS unique_id
+(
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
