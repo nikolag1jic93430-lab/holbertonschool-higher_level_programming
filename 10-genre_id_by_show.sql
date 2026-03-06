a742847ed5c971dab518e4dda9f1c852e54a6eaf
nikolag1jic93430-lab <nikolag1jic93430@gmail.com>
2026-03-06 12:40:07 +0100

    Delete 10-genre_id_by_show.sql

--- 10-genre_id_by_show.sql
+++
@@ -1,4 +0,0 @@
--- show all shows that have one genre linked
-SELECT title, tv_show_genres.genre_id
-FROM tv_shows FULL JOIN tv_show_genres ON id = tv_show_genres.show_id
-ORDER BY title, tv_show_genres.genre_id;
